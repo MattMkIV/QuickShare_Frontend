f25408c4a05f9750114b81a53f1e0e34ac1bd410
Mattia <mattialzr1@gmail.com>
2023-05-31 22:17:42 +0200

    LeftBar selection animation

--- src/Components/homepage/LeftBar.tsx
+++ src/Components/homepage/LeftBar.tsx
@@ -12,7 +12,7 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 //CSS
 import './LeftBar.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Grid from "@mui/material/Grid";
 import {AddPhotoAlternate} from '@mui/icons-material';
 import {Box, Dialog} from "@mui/material";
@@ -61,6 +61,12 @@ function LeftBar({onSelect}: any) {
 
     const navigate = useNavigate();
 
+    const [selectedItem, setSelectedItem] = useState('home');
+
+    const handleItemClick = (item: any) => {
+        setSelectedItem(item);
+    };
+
     //Function
 
     return (
@@ -72,43 +78,78 @@ function LeftBar({onSelect}: any) {
             />
 
             <Grid sx={{display: {xs: 'none', md: 'block'}}} className='leftBarGrid'>
-                <Button className='buttonNew' style={{background: '#920609', justifyContent: "flex-start",
-                    color: '#ffdad5'}} variant="contained" startIcon={<AddIcon/>}
-                        onClick={handleClickOpen}>New</Button>
+                <Button
+                    sx={{
+                        fontFamily: 'Roboto Bold', height: '70px', width: '96%', marginLeft: '10px',
+                        fontSize: '20px', textAlign: 'left', borderRadius: '22px'
+                    }}
+                    style={{background: '#920609', justifyContent: "flex-start", color: '#ffdad5'}}
+                    variant="contained" startIcon={<AddIcon/>}
+                    onClick={handleClickOpen}>New</Button>
 
                 <List sx={{width: '100%', color: 'white'}}>
-                    <ListItem className='noPaddingTopBottom' onClick={() => navigate("/homepage")}>
-                        <ListItemButton style={{color : '#ffb4aa'}} className='clickAnimation'>
+                    <ListItem className='noPaddingTopBottom' onClick={function (event) {
+                        navigate("/homepage");
+                        handleItemClick("home");
+                    }}>
+                        <ListItemButton
+                            style={{color: '#ffb4aa'}}
+                            className={selectedItem === "home" ? "selected" : ""}
+                            sx={{marginLeft: '-16px', borderRadius: '0 22px 22px 0', marginRight: '-16px'}}>
                             <HomeIcon className='icon'/>
                             <ListItemText primary="Home"/>
                         </ListItemButton>
                     </ListItem>
-                    <ListItem className='noPaddingTopBottom' onClick={() => navigate("/homepage/notes")}>
-                        <ListItemButton style={{color : '#ffb4aa'}} className='clickAnimation'>
+                    <ListItem className='noPaddingTopBottom' onClick={function (event) {
+                        navigate("/homepage/notes");
+                        handleItemClick("notes");
+                    }}>
+                        <ListItemButton
+                            style={{color: '#ffb4aa'}}
+                            className={selectedItem === "notes" ? "selected" : ""}
+                            sx={{marginLeft: '-16px', borderRadius: '0 22px 22px 0', marginRight: '-16px'}}>
                             <AssignmentIcon className='icon'/>
                             <ListItemText primary="Notes"/>
                         </ListItemButton>
                     </ListItem>
-                    <ListItem className='noPaddingTopBottom' onClick={() => navigate("/homepage/lists")}>
-                        <ListItemButton style={{color : '#ffb4aa'}} className='clickAnimation'>
+                    <ListItem className='noPaddingTopBottom' onClick={function (event) {
+                        navigate("/homepage/lists");
+                        handleItemClick("lists");
+                    }}>
+                        <ListItemButton
+                            style={{color: '#ffb4aa'}}
+                            className={selectedItem === "lists" ? "selected" : ""}
+                            sx={{marginLeft: '-16px', borderRadius: '0 22px 22px 0', marginRight: '-16px'}}>
                             <FormatListBulletedIcon className='icon'/>
                             <ListItemText primary="Lists"/>
                         </ListItemButton>
                     </ListItem>
-                    <ListItem className='noPaddingTopBottom' onClick={() => navigate("/homepage/photo")}>
-                        <ListItemButton style={{color : '#ffb4aa'}} className='clickAnimation'>
+                    <ListItem className='noPaddingTopBottom' onClick={function (event) {
+                        navigate("/homepage/photo");
+                        handleItemClick("photo");
+                    }}>
+                        <ListItemButton
+                            style={{color: '#ffb4aa'}}
+                            className={selectedItem === "photo" ? "selected" : ""}
+                            sx={{marginLeft: '-16px', borderRadius: '0 22px 22px 0', marginRight: '-16px'}}>
                             <AddPhotoAlternate className='icon'/>
                             <ListItemText primary="Photos"/>
                         </ListItemButton>
                     </ListItem>
-                    <ListItem className='noPaddingTopBottom' onClick={() => onSelect('Calendar')}>
-                        <ListItemButton style={{color : '#ffb4aa'}} className='clickAnimation'>
+                    <ListItem className='noPaddingTopBottom' onClick={function (event) {
+                        navigate("/homepage/calendar");
+                        handleItemClick("calendar");
+                    }}>
+                        <ListItemButton
+                            style={{color: '#ffb4aa'}}
+                            className={selectedItem === "calendar" ? "selected" : ""}
+                            sx={{marginLeft: '-16px', borderRadius: '0 22px 22px 0', marginRight: '-16px'}}>
                             <DateRangeIcon className='icon'/>
                             <ListItemText primary="Calendar "/>
                         </ListItemButton>
                     </ListItem>
                     <ListItem className='noPaddingTopBottom' onClick={() => onSelect('AccountBalance')}>
-                        <ListItemButton style={{color : '#ffb4aa'}} className='clickAnimation'>
+                        <ListItemButton style={{color: '#ffb4aa'}} className='clickAnimation'>
                             <AccountBalanceWalletIcon className='icon'/>
                             <ListItemText primary="Account balance"/>
                         </ListItemButton>
