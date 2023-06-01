ea38e2ca8010cb2a18a7476ea931fceb6efe579a
Mattia <mattialzr1@gmail.com>
2023-06-02 00:37:55 +0200

    Added CardLayout confirmation box while editing

--- src/Components/homepage/CardLayout.tsx
+++ src/Components/homepage/CardLayout.tsx
@@ -1,4 +1,4 @@
-import {Box, CardContent, Fab, Slide, TextField, Typography} from "@mui/material";
+import {CardContent, Fab, Grow, Slide, TextField, Typography} from "@mui/material";
 import Card from "@mui/material/Card";
 import * as React from "react";
 import InfoIcon from '@mui/icons-material/Info';
@@ -6,23 +6,12 @@ import ShareIcon from '@mui/icons-material/Share';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import './CardLayout.css'
-import {styled} from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
 
-function CardLayout() {
-    const CssTextField = styled(TextField)({
-        '& .MuiOutlinedInput-notchedOutline': {
-            border: 'none',
-        },
-        '& .MuiOutlinedInput-root': {
-            '&:hover fieldset': {
-                borderRadius: 12,
-            },
-            '&.Mui-focused fieldset': {
-                borderRadius: 12,
-            },
-        },
-    });
 
+const CardLayout: React.FC = () => {
     const [isHovered, setIsHovered] = React.useState(false);
 
     const handleMouseEnter = () => {
@@ -33,10 +22,31 @@ function CardLayout() {
         setIsHovered(false);
     };
 
+    const [isEditable, setIsEditable] = React.useState(false);
+    const [textFieldValue, setTextFieldValue] = React.useState('');
+
+    const handleTextFieldClick = () => {
+        setIsEditable(true);
+    };
+
+    const handleConfirmClick = () => {
+        // Aggiungi qui la logica per confermare la modifica
+        setIsEditable(false);
+    };
+
+    const handleCloseClick = () => {
+        // Aggiungi qui la logica per confermare la modifica
+        setIsEditable(false);
+    };
+
+    const handleTextFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTextFieldValue(event.target.value);
+    };
+
     return (
         <>
             <Card className='cardsLayout'>
-                <CardContent sx={{ m: -1 }}
+                <CardContent sx={{m: -1}}
                              onMouseEnter={handleMouseEnter}
                              onMouseLeave={handleMouseLeave}>
                     <Typography noWrap className='cardTitle'>
@@ -45,38 +55,70 @@ function CardLayout() {
 
                     <hr className='separationLine'></hr>
 
-                    <Box sx={{height: '290px', overflowY : 'scroll'}}>
-                        <CssTextField
-                            multiline
-                            sx={{fontFamily : 'Roboto Regular', width : '100%', height : '290px'}}
-                            rows={12}
-                            inputProps={{
-                                sx: {
-                                    color: '#574419 !important',
-                                },
-                            }}
-                        />
-                    </Box>
+                    <TextField
+                        multiline
+                        sx={{
+                            fontFamily: 'Roboto Regular', width: '100%', height: '290px',
+                            '& fieldset': {border: 'none'}
+                        }}
+                        rows={12}
+                        inputProps={{
+                            sx: {
+                                color: '#574419 !important',
+                            },
+                        }}
+                        value={textFieldValue}
+                        onClick={handleTextFieldClick}
+                        onChange={handleTextFieldChange}
+                        disabled={!isEditable}
+                    />
 
-                    <Slide direction="up" in={isHovered} mountOnEnter unmountOnExit timeout={100}>
-                        <Fab sx={{backgroundColor : '#dfc38c', marginLeft : '27px', marginRight : '25px',
-                            ':hover' : {backgroundColor : '#deba7b'} }}>
-                            <InfoIcon sx={{color : '#3f2e04'}} />
-                        </Fab>
-                    </Slide>
+                    {isEditable ? (
+                        <Grid>
+                            <Grow in={isHovered} mountOnEnter unmountOnExit timeout={100}>
+                                <Fab onClick={handleCloseClick} sx={{
+                                    backgroundColor: '#ffb4aa', marginLeft: '55px', marginRight: '25px',
+                                    ':hover': {backgroundColor: '#fda498'}
+                                }}>
+                                    <CloseIcon sx={{color: '#690003'}}/>
+                                </Fab>
+                            </Grow>
 
-                    <Slide direction="up" in={isHovered} mountOnEnter unmountOnExit timeout={200}>
-                        <Fab sx={{backgroundColor : '#e7bdb7',  marginRight : '25px',
-                            ':hover' : {backgroundColor : '#e3ada5'}}}>
-                            <ShareIcon sx={{color : '#442926'}} />
-                        </Fab>
-                    </Slide>
+                            <Grow in={isHovered} mountOnEnter unmountOnExit timeout={200}>
+                                <Fab onClick={handleConfirmClick} sx={{
+                                    backgroundColor: '#65cc8f', marginLeft: '27px', marginRight: '25px',
+                                    ':hover': {backgroundColor: '#58b27f'}
+                                }}>
+                                    <DoneIcon sx={{color: '#285740'}}/>
+                                </Fab>
+                            </Grow>
 
-                    <Slide direction="up" in={isHovered} mountOnEnter unmountOnExit timeout={400}>
-                        <Fab sx={{backgroundColor : '#ffb4aa', ':hover' : {backgroundColor : '#fda498'} }}>
-                            <DeleteIcon sx={{color : '#690003'}} />
-                        </Fab>
-                    </Slide>
+                        </Grid>
+                    ) : (
+                        <Grid>
+                            <Slide direction="up" in={isHovered} mountOnEnter unmountOnExit timeout={100}>
+                                <Fab sx={{
+                                    backgroundColor: '#dfc38c', marginLeft: '27px', marginRight: '25px',
+                                    ':hover': {backgroundColor: '#deba7b'}
+                                }}>
+                                    <InfoIcon sx={{color: '#3f2e04'}}/>
+                                </Fab>
+                            </Slide>
+                            <Slide direction="up" in={isHovered} mountOnEnter unmountOnExit timeout={200}>
+                                <Fab sx={{
+                                    backgroundColor: '#e7bdb7', marginRight: '25px',
+                                    ':hover': {backgroundColor: '#e3ada5'}
+                                }}>
+                                    <ShareIcon sx={{color: '#442926'}}/>
+                                </Fab>
+                            </Slide>
+                            <Slide direction="up" in={isHovered} mountOnEnter unmountOnExit timeout={400}>
+                                <Fab sx={{backgroundColor: '#ffb4aa', ':hover': {backgroundColor: '#fda498'}}}>
+                                    <DeleteIcon sx={{color: '#690003'}}/>
+                                </Fab>
+                            </Slide>
+                        </Grid>
+                    )}
                 </CardContent>
             </Card>
         </>
