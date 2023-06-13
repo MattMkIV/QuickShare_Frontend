7bb93752d2b07e3b89655ff8059282a5d8ebd919
Mattia <mattialzr1@gmail.com>
2023-06-13 12:23:21 +0200

    Added all 3 button pop-up

--- src/Components/homepage/NoteCardLayout.tsx
+++ src/Components/homepage/NoteCardLayout.tsx
@@ -1,5 +1,6 @@
 import {
     Box,
+    Button,
     CardContent,
     Fab,
     Grow,
@@ -17,6 +18,7 @@ import {useState} from "react";
 import InfoIcon from '@mui/icons-material/Info';
 import ShareIcon from '@mui/icons-material/Share';
 import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
 
 import './NoteCardLayout.css'
 import Grid from "@mui/material/Grid";
@@ -81,9 +83,28 @@ const NoteCardLayout: React.FC = () => {
         setAnchorEl(null);
     };
 
+    /************************* Share pop up TextField *************************/
+    const [textFields, setTextFields] = useState<string[]>([]);
+
+    const handleAddTextField = () => {
+        setTextFields([...textFields, '']);
+    };
+
+    const handleRemoveTextField = (index: number) => {
+        const updatedTextFields = [...textFields];
+        updatedTextFields.splice(index, 1);
+        setTextFields(updatedTextFields);
+    };
+
+    const handleChangeTextField = (index: number, value: string) => {
+        const updatedTextFields = [...textFields];
+        updatedTextFields[index] = value;
+        setTextFields(updatedTextFields);
+    };
+
     return (
         <>
-            <Card className='cardsLayout' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <Card onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='cardsLayout'>
                 <CardContent sx={{m: -1}}>
                     <TextField
                         inputProps={{
@@ -162,17 +183,17 @@ const NoteCardLayout: React.FC = () => {
                                 <Fab sx={{
                                     backgroundColor: '#dfc38c', marginLeft: '27px', marginRight: '25px',
                                     ':hover': {backgroundColor: '#deba7b'}
-                                }} onClick={(event) => topBarClick(event, 'notification')}
-                                     aria-controls='notification'
+                                }} onClick={(event) => topBarClick(event, 'info')}
+                                     aria-controls='info'
                                      aria-haspopup='true'>
                                     <InfoIcon sx={{color: '#3f2e04'}}/>
                                 </Fab>
                             </Slide>
 
                             <Menu
-                                id='notification'
+                                id='info'
                                 anchorEl={anchorEl}
-                                open={Boolean(anchorEl && menuId === 'notification')}
+                                open={Boolean(anchorEl && menuId === 'info')}
                                 onClose={handleClose}
                                 anchorOrigin={{
                                     vertical: 'top',
@@ -200,7 +221,7 @@ const NoteCardLayout: React.FC = () => {
                                                 marginTop: '5px',
                                                 color: '#3f2e04'
                                             }}>
-                                    Created in:
+                                    Created on:
                                 </Typography>
                                 <Typography component="span" display="inline-block" whiteSpace="nowrap"
                                             sx={{
@@ -223,7 +244,7 @@ const NoteCardLayout: React.FC = () => {
                                     width: '100%',
                                     height: '237px',
                                     borderRadius: '22px',
-                                    backgroundColor: '#b2945f',
+                                    backgroundColor: '#d9b267',
                                     overflowY: 'scroll',
                                     pl: 1.2, pr: 1.2, pt: 1.2
                                 }}>
@@ -249,15 +270,16 @@ const NoteCardLayout: React.FC = () => {
                                                    '& .MuiInputBase-input': {
                                                        fontFamily: 'Roboto Regular',
                                                        fontSize: '15px !important',
-                                                       height: '10px',
+                                                       height: '5px',
                                                        width: '202px',
+                                                       borderRadius: '18px',
+                                                       boxShadow: 5,
                                                    },
                                                    marginBottom: 1.2
                                                }}
                                                defaultValue='ciaocarlo@gmail.com'
                                                disabled>
                                     </TextField>
-
                                 </Box>
                             </Menu>
 
@@ -292,8 +314,108 @@ const NoteCardLayout: React.FC = () => {
                                         height: '320px',
                                         borderRadius: '22px',
                                         backgroundColor: '#e7bdb7',
+                                        overflowY: 'hidden'
                                     }
                                 }}>
+                                <Typography sx={{
+                                    fontFamily: 'Roboto Black',
+                                    fontSize: '17px',
+                                    marginLeft: '15px',
+                                    marginTop: '5px',
+                                    color: '#3f2e04',
+                                }}>Share with:</Typography>
+                                <Box sx={{
+                                    width: '100%',
+                                    height: '220px',
+                                    borderRadius: '22px',
+                                    backgroundColor: '#eaa79d',
+                                    pl: 1.2, pr: 1.2, pt: 1.2,
+                                    overflowY: 'scroll',
+                                }}>
+                                    {textFields.map((textField, index) => (
+                                        <div key={index} style={{display: 'flex'}}>
+                                            <TextField
+                                                inputProps={{
+                                                    sx: {color: '#3f2e04 !important'}
+                                                }}
+                                                sx={{
+                                                    '& .MuiInput-underline': {
+                                                        borderBottomColor: 'transparent',
+                                                    },
+                                                    '& .MuiOutlinedInput-root': {
+                                                        '& fieldset': {
+                                                            borderColor: '#3f2e04',
+                                                            borderRadius: '18px',
+                                                        },
+                                                        '&:hover fieldset': {
+                                                            borderColor: '#3f2e04',
+                                                        },
+                                                        '&.Mui-focused fieldset': {
+                                                            borderColor: '#3f2e04',
+                                                            borderWidth: '2px',
+                                                        },
+                                                    },
+                                                    '& .MuiInputBase-input': {
+                                                        borderRadius: '18px',
+                                                        fontFamily: 'Roboto Regular',
+                                                        fontSize: '15px !important',
+                                                        height: '5px',
+                                                        width: '202px',
+                                                        boxShadow: 5,
+                                                    },
+                                                    marginBottom: 1.2
+                                                }}
+                                            />
+                                            <Button onClick={() => handleRemoveTextField(index)}
+                                                    sx={{
+                                                        backgroundColor: '#920609',
+                                                        height: '30px',
+                                                        minWidth: '30px',
+                                                        borderRadius: '22px',
+                                                        marginLeft: '10px',
+                                                        marginTop: '3px',
+                                                        boxShadow: 8,
+                                                        ':hover': {backgroundColor: '#9f3a3c'}
+                                                    }}
+                                                    disableRipple>
+                                                <DeleteIcon sx={{height: '15px', width: '15px', color: '#ffb4aa'}}/>
+                                            </Button>
+                                        </div>
+                                    ))}
+                                </Box>
+                                <Grid sx={{
+                                    width: '100%',
+                                    marginTop: '10px',
+                                    display: 'flex',
+                                    justifyContent: 'flex-end',
+                                    alignContent: 'center',
+                                    pr: 1.2
+                                }}>
+                                    <Button sx={{
+                                        minWidth: '40px',
+                                        height: '40px',
+                                        boxShadow: 8,
+                                        backgroundColor: '#8fb677',
+                                        borderRadius: '30px',
+                                        ':hover': {backgroundColor: '#7a9a65'}
+                                    }} disableRipple onClick={handleAddTextField}>
+                                        <AddIcon sx={{color: '#201a19'}}></AddIcon>
+                                    </Button>
+                                    <Button sx={{
+                                        minWidth: '90px',
+                                        height: '40px',
+                                        marginLeft: '92px',
+                                        boxShadow: 8,
+                                        backgroundColor: '#dfc38c',
+                                        borderRadius: '30px',
+                                        fontFamily: 'Roboto Regular',
+                                        fontSize: '14px',
+                                        ':hover': {backgroundColor: '#c7ad7b'},
+                                        color: '#201a19',
+                                    }} disableRipple onClick={handleAddTextField}>
+                                        Share!
+                                    </Button>
+                                </Grid>
                             </Menu>
 
                             <Slide direction="up" in={isHovered} mountOnEnter unmountOnExit timeout={400}>
@@ -324,11 +446,47 @@ const NoteCardLayout: React.FC = () => {
                                     elevation: 24,
                                     style: {
                                         width: '250px',
-                                        height: '320px',
+                                        height: '108px',
                                         borderRadius: '22px',
                                         backgroundColor: '#ffb4aa',
                                     }
                                 }}>
+                                <Typography sx={{
+                                    fontFamily: 'Roboto Black',
+                                    fontSize: '17px',
+                                    marginLeft: '15px',
+                                    marginTop: '5px',
+                                    color: '#3f2e04'
+                                }}>Delete permanently?</Typography>
+                                <Box sx={{backgroundColor: '#fc8e82', height: '77px', borderRadius: '22px'}}>
+                                    <Grid sx={{display: 'flex', justifyContent: 'center'}}>
+                                        <Button sx={{
+                                            boxShadow: 8,
+                                            height: '45px',
+                                            minWidth: '85px',
+                                            borderRadius: '22px',
+                                            backgroundColor: '#5d3f3b',
+                                            color: '#ffdad5',
+                                            fontFamily: 'Roboto Regular',
+                                            fontSize: '15px',
+                                            marginTop: '10px',
+                                            ':hover': {backgroundColor: '#49302b'}
+                                        }} disableRipple>No</Button>
+                                        <Button sx={{
+                                            boxShadow: 8,
+                                            height: '45px',
+                                            minWidth: '85px',
+                                            borderRadius: '22px',
+                                            backgroundColor: '#920609',
+                                            color: '#ffdad5',
+                                            fontFamily: 'Roboto Regular',
+                                            fontSize: '15px',
+                                            marginLeft: '20px',
+                                            marginTop: '10px',
+                                            ':hover': {backgroundColor: '#7e0508'}
+                                        }} disableRipple>Yes</Button>
+                                    </Grid>
+                                </Box>
                             </Menu>
                         </Box>
                     )}
