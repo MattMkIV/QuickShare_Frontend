a1052335302ba95d2f7ba3fe92237003d15033c3
andreagrassi11 <andreagrassi200a@gmail.com>
2023-06-14 17:27:00 +0200

    note

--- src/Pages/View/notes/Notes.tsx
+++ src/Pages/View/notes/Notes.tsx
@@ -14,6 +14,7 @@ function Notes() {
     let jwtError = false;
     let navigate = useNavigate();
     const [notes, setNotes] = useState<any>([]);
+    const [previusDate, setPreviusDate ] = useState<String>("");
 
     useEffect(() => {
         const check = async () => {
@@ -43,30 +44,53 @@ function Notes() {
                     <hr className='dateLineCentralContent'></hr>
                 </Grid>
 
-                <Grid item className='cardSliderHomePage'>
-                    <Stack spacing={5} direction="row">
-                    {notes.map((note:any, index:any) => (
-                        <CardLayout key={index} title={note.title} noteId={note.note_id} createData={note.create_date} body={note.body} allowed={note.allowed}></CardLayout>
-                    ))} 
-                    </Stack>
-                </Grid>
+                {notes.map((note:any, index:any) => {
+                    (previusDate === note.create_date) ? 
+                        (
+                            <CardLayout key={index} title={note.title} noteId={note.note_id} createData={note.create_date} body={note.body} allowed={note.allowed}></CardLayout>
+                        )
+                    : 
+                        (
+                                </Stack>
+                            </Grid>
+                            <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
+                                <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>20/01/2023</Typography>
+                                <hr className='dateLineCentralContent'></hr>
+                            </Grid>
+                            <Grid item className='cardSliderHomePage'>
+                                <Stack spacing={5} direction="row">
+                        )
+                    
+                        setPreviusDate(note.create_date);
+                })} 
+                {/* // <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
+                //     <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>20/01/2023</Typography>
+                //     <hr className='dateLineCentralContent'></hr>
+                // </Grid>
 
-                <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
-                    <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>20/01/2023</Typography>
-                    <hr className='dateLineCentralContent'></hr>
-                </Grid>
+                // <Grid item className='cardSliderHomePage'>
+                //     <Stack spacing={5} direction="row">
+                //         <CardLayout key={index} title={note.title} noteId={note.note_id} createData={note.create_date} body={note.body} allowed={note.allowed}></CardLayout>
+                        
+                //     </Stack>
+                // </Grid>
+
+                // <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
+                //     <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>20/01/2023</Typography>
+                //     <hr className='dateLineCentralContent'></hr>
+                // </Grid>
 
 
-                <Grid item className='cardSliderHomePage'>
-                    <Stack spacing={5} direction="row">
-                        {/* <CardLayout></CardLayout>
-                        <CardLayout></CardLayout>
-                        <CardLayout></CardLayout>
-                        <CardLayout></CardLayout>
-                        <CardLayout></CardLayout>
-                        <CardLayout></CardLayout> */}
-                    </Stack>
-                </Grid>
+                // <Grid item className='cardSliderHomePage'>
+                //     <Stack spacing={5} direction="row">
+                //         {/* <CardLayout></CardLayout>
+                //         <CardLayout></CardLayout>
+                //         <CardLayout></CardLayout>
+                //         <CardLayout></CardLayout>
+                //         <CardLayout></CardLayout>
+                //         <CardLayout></CardLayout> 
+                //     </Stack>
+                // </Grid> */}
             </Box>
         </>
     );
