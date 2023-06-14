0206a5c7b8231f85e6be34326dc06b68c63a73f0
andreagrassi11 <andreagrassi200a@gmail.com>
2023-06-14 19:06:44 +0200

    note

--- src/Pages/View/notes/Notes.tsx
+++ src/Pages/View/notes/Notes.tsx
@@ -24,7 +24,30 @@ function Notes() {
 
         const takeNotes = async () => {
             let response:any = await TakeNote();
-            setNotes(response);
+
+            let allNotes = [];
+            let currentDate = [];
+            let previusDate = "0000-00-00";
+
+            for(let i=0; i < response.length; i++) {
+
+                console.log("rgerger " + response[i]);
+
+                if(previusDate === response[i].create_date) {
+                    currentDate.push(response[i]);
+                    previusDate = response[i].create_date;
+                } else {
+                    if(i !== 0)
+                        allNotes.push(currentDate);
+                    
+                    currentDate = [];
+                    currentDate.push(response[i]);
+                    previusDate = response[i].create_date;
+                }
+            }
+            allNotes.push(currentDate);
+            console.log(allNotes);
+            setNotes(allNotes);
         }   
 
         check();
@@ -39,58 +62,22 @@ function Notes() {
                     <h1 className='titleSection'>Notes:</h1>
                 </Grid>
 
-                <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
-                    <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>20/01/2023</Typography>
-                    <hr className='dateLineCentralContent'></hr>
-                </Grid>
+                {notes.map((note:any, index:any) => ( 
+                    <>
+                        <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
+                            <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>{note[index].create_date}</Typography>
+                            <hr className='dateLineCentralContent'></hr>
+                        </Grid>
 
-                {notes.map((note:any, index:any) => {
-                    (previusDate === note.create_date) ? 
-                        (
-                            <CardLayout key={index} title={note.title} noteId={note.note_id} createData={note.create_date} body={note.body} allowed={note.allowed}></CardLayout>
-                        )
-                    : 
-                        (
-                                </Stack>
-                            </Grid>
-                            <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
-                                <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>20/01/2023</Typography>
-                                <hr className='dateLineCentralContent'></hr>
-                            </Grid>
-                            <Grid item className='cardSliderHomePage'>
-                                <Stack spacing={5} direction="row">
-                        )
-                    
-                        setPreviusDate(note.create_date);
-                })} 
-                {/* // <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
-                //     <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>20/01/2023</Typography>
-                //     <hr className='dateLineCentralContent'></hr>
-                // </Grid>
-
-                // <Grid item className='cardSliderHomePage'>
-                //     <Stack spacing={5} direction="row">
-                //         <CardLayout key={index} title={note.title} noteId={note.note_id} createData={note.create_date} body={note.body} allowed={note.allowed}></CardLayout>
-                        
-                //     </Stack>
-                // </Grid>
-
-                // <Grid wrap='nowrap' sx={{display : 'flex', alignItems : 'center', justifyContent : 'flex-start'}}>
-                //     <Typography sx={{fontFamily : 'Roboto Bold', fontSize : '12px', color : '#dfc38c', marginRight : '10px', marginLeft : '25px'}}>20/01/2023</Typography>
-                //     <hr className='dateLineCentralContent'></hr>
-                // </Grid>
-
-
-                // <Grid item className='cardSliderHomePage'>
-                //     <Stack spacing={5} direction="row">
-                //         {/* <CardLayout></CardLayout>
-                //         <CardLayout></CardLayout>
-                //         <CardLayout></CardLayout>
-                //         <CardLayout></CardLayout>
-                //         <CardLayout></CardLayout>
-                //         <CardLayout></CardLayout> 
-                //     </Stack>
-                // </Grid> */}
+                        <Grid item className='cardSliderHomePage'>
+                            <Stack spacing={5} direction="row">
+                                {note.map((n:any, i:any) => (                    
+                                    <CardLayout key={i} title={n.title} noteId={n.note_id} createData={n.create_date} body={n.body} allowed={n.allowed}></CardLayout>
+                                ))} 
+                            </Stack>
+                        </Grid>
+                    </>
+                ))}
             </Box>
         </>
     );
